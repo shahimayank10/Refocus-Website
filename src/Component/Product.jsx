213eedd34a0ec450954ec62b02cb6b3e882d1a4f
shahimayank10 <shahimayank10@gmail.com>
2025-04-02 10:56:32 +0530

    Streap responsiveness

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -4,7 +4,7 @@ import Button from './Button'
 const Product = ({val}) => {
   return (
     <div className='w-full py-20'>
-    <div className='max-w-screen-xl m-auto py-5 px-8 flex items-center justify-between '>
+    <div className='max-w-screen-xl m-auto py-5 px-8 flex flex-wrap items-center justify-between '>
     <h1 className='text-6xl capitalize font-medium'>{val.name}</h1>
     <div id="content" className=' w-92 flex flex-col '>
     <p>Lorem ipsum dolor sit, amet consectetur adipi elit. Ratione et delectus, dolore culpa magnam modi in deserunt. Praesentium fugit quos, cum eaque ipsam impedit. Debitis pariatur, id vel temporibus veritatis ratione ipsam dicta minus animi explicabo beatae voluptatum ipsum illo!</p>
--- src/Component/Streap.jsx
+++ src/Component/Streap.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Streap = ({val}) => {
   return (
-        <div  className='flex items-center w-[20%] no-wrap border-zinc-600  border-t-[1px] border-r-[1px] border-b-[1px] p-3  justify-between'>
+        <div  className='flex items-center w-[20%] flex-wrap border-zinc-600  border-t-[1px] border-r-[1px] border-b-[1px] p-3  justify-between'>
          <img className='w-15' src={val.image}/>
          <span className=''>{val.num}</span>
         </div>
